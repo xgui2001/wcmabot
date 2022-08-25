070c3b2b7b319dd7ed3838e15b42c0eebb75871c
Xinyi (Angela) Gui <80857236+xgui2001@users.noreply.github.com>
2022-08-25 03:39:18 -0400

    Add comments
    Updated wcma dataset csv can be found at https://github.com/wcmaart/collection

---
+++ helper.py
@@ -0,0 +1,22 @@
+import os
+import random
+import pandas as pd
+
+
+# import WCMA collection data from csv file and stores it with a Pandas dataframe
+def get_collection():
+    data_dir = os.getcwd()+'/collection'
+    collection_df = pd.read_csv(
+        data_dir+'/wcma-collection.csv', low_memory=False)
+    return collection_df
+
+
+# generate a random object from the WCMA dataframe
+def generate_object():
+    random_int = random.randint(0, len(get_collection()))
+    random_object = get_collection().iloc[random_int]
+    # one object has title, maker, creation date, medium and departmet
+    object_string = str(random_object[1]) + ', ' + str(random_object[2]) + ', ' + str(random_object[7]) + \
+        ', ' + str(random_object[12]) + ', ' + str(random_object[4])
+    print(object_string)
+    return object_string
