f88480b82b810bd96c2b5317d05b6d78562aec3f
Xinyi (Angela) Gui <80857236+xgui2001@users.noreply.github.com>
2022-08-25 03:01:49 -0400

    Initial commit
    Most updated dataset of WCMA's collection could be accessed at https://github.com/wcmaart/collection.

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+import tweepy
+import helper
+
+wcma_url = 'https://artmuseum.williams.edu/collection/'
+wcma_hashtag = '#WilliamsCollegeMuseumOfArt'
+
+
+def get_api():
+    all_keys = open('twitterkeys', 'r').read().splitlines()
+    api_key = all_keys[0]
+    api_key_secret = all_keys[1]
+    bearer_token = all_keys[2]
+    access_token = all_keys[3]
+    access_token_secret = all_keys[4]
+
+    auth = tweepy.OAuthHandler(api_key, api_key_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    return tweepy.API(auth, wait_on_rate_limit=True)
+
+
+def tweet(api: tweepy.API, message: str, image_path=None):
+    if image_path:
+        api.update_status_with_media(message, image_path)
+    else:
+        api.update_status(message)
+    return 0
+
+
+if __name__ == "__main__":
+    api = get_api()
+    try:
+        api.verify_credentials()
+        print("Authentication OK")
+    except:
+        print("Error during authentication")
+    while True:
+        tweet(api, helper.generate_object() +
+              ' ' + wcma_hashtag + ' ' + wcma_url)
+        time.sleep(600)
